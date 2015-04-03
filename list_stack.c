9c62f789313c1dff1b14c2ed53433c22186817c5
DengZuoheng <dengzuoheng@gmail.com>
2015-04-04 00:08:55 +0800

    add list_stack

---
+++ list_stack.c
@@ -0,0 +1,36 @@
+#include "list_stack.h"
+#include "default_setting.h"
+#include "list.h"
+
+bool list_stack_push(list_stack_p stack, void* data, size_t n)
+{
+        list_push_back(stack->list, data, n);
+        return true;
+}
+
+bool list_stack_pop(list_stack_p stack)
+{
+        list_pop_back(stack->list);
+}
+/*Get the top item of the stack. */
+void* list_stack_top(list_stack_p stack)
+{
+        return list_back(stack->list);
+}
+/*Test whether the stack is empty or not. */
+bool list_stack_empty(list_stack_p stack)
+{
+        return list_empty(stack->list);
+}
+/*Test whether the stack is full or not. */
+size_t list_stack_size(list_stack_p stack)
+{
+        return list_size(stack->list);
+}
+/*Destory the stack and free all the memory associated with it. */
+void* list_stack_destroy(list_stack_p stack)
+{
+        list_destroy(stack->list);
+        free(stack);
+        return NULL;
+}
---
+++ list_stack.h
@@ -0,0 +1,25 @@
+#ifndef LIST_STACK_H
+#define LIST_STACK_H
+#include "list.h"
+#include "default_setting.h"
+
+struct list_stack
+{
+    list_p list;
+};
+
+typedef struct list_stack* list_stack_p;
+
+bool list_stack_push(list_stack_p stack, void* data, size_t n);
+
+bool list_stack_pop(list_stack_p stack);
+/*Get the top item of the stack. */
+void* list_stack_top(list_stack_p stack);
+/*Test whether the stack is empty or not. */
+bool list_stack_empty(list_stack_p stack);
+/*Test whether the stack is full or not. */
+size_t list_stack_size(list_stack_p stack);
+/*Destory the stack and free all the memory associated with it. */
+void* list_stack_destroy(list_stack_p stack);
+
+#endif
